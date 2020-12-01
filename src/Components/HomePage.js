d57cb0afb71a0a95a4ec801f0539b1e3e6c4d3e4
ICX\Dzmitry.Nekrasau <Dzmitry.Nekrasau@itechart-group.com>
2020-12-01 19:21:06 +0300

    add alert

--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -1,11 +1,13 @@
 import React, { useCallback } from 'react';
 import 'Styles/WelcomeStyle.css';
 import { makeStyles } from '@material-ui/core/styles';
-import { AppBar, Box, Container, Toolbar, Typography } from '@material-ui/core';
+import { AppBar, Box, Collapse, Container, Toolbar, Typography } from '@material-ui/core';
 import  Button  from '@material-ui/core/Button';
 import axios from 'axios';
 import { useHistory } from 'react-router-dom';
 import Alert from '@material-ui/lab/Alert';
+import IconButton from '@material-ui/core/IconButton';
+import CloseIcon from '@material-ui/icons/Close';
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -23,6 +25,8 @@ function HomePage() {
     
     const history = useHistory();
     const classes = useStyles();
+
+    const [open, setOpen] = React.useState(true);
 
     const handleGetProfile = () => {
 
@@ -63,13 +67,26 @@ function HomePage() {
                             </Toolbar>
                         </Container>
                         <Container>
-                        <Alert severity="success">
+                            <Collapse in={open}>
+                        <Alert severity="success" action={<IconButton aria-label="close" color="inherit" size="small" onClick= { () => { setOpen(false); } }> <CloseIcon fontSize="inherit" /> </IconButton>}>
                             you have successfully logged in
                         </Alert>
+                        </Collapse>
                         </Container>
                     </AppBar>
                     </div>
                 </div>
+
+
+
+
+
+
+
+
+
+
+
 
     );
 
